6a9e4c97041a05f1aa06371dc804a51e0009bb67
Heck <zgzamoshi@163.com>
2015-10-25 17:30:41 +0800

    添加粒子系统两大模式下，格子的私有属性修改接口
    Signed-off-by: Heck <zgzamoshi@163.com>

--- Classes/UI/AliceParticle2DSystem.cpp
+++ Classes/UI/AliceParticle2DSystem.cpp
@@ -18,16 +18,127 @@ int AliceParticle2DSystem_AddChild(lua_State*L)
 	return 0;
 }
 
+int AliceParticle2DSystem_ChangeMode(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	int mode = lua_tointeger(L, 2);//0、kCCParticleModeGravity，1、kCCParticleModeRadius
+	((ParticleSystemQuad*)ptr->node)->setEmitterMode((ParticleSystem::Mode)mode);
+	
+	return 0;
+}
+
+//在重力模式下才有效的属性值
+int AliceParticle2DSystem_SetGravity(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setGravity(ccp(lua_tonumber(L,2),lua_tonumber(L,3)));
+	return 0;
+}
+int AliceParticle2DSystem_SetSpeed(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setSpeed(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetSpeedVar(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setSpeedVar(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetTangencialAccel(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setTangentialAccel(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetTangencialAccelVar(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setTangentialAccelVar(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetRadialAccel(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setRadialAccel(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetRadialAccelVar(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setRadialAccelVar(lua_tonumber(L, 2));
+	return 0;
+}
+
+//角度模式
+int AliceParticle2DSystem_SetStartRadius(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setStartRadius(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetStartRadiusVar(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setStartRadiusVar(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetEndRadius(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setEndRadius(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetEndRadiusVar(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setEndRadiusVar(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetRotatePerSecond(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setRotatePerSecond(lua_tonumber(L, 2));
+	return 0;
+}
+int AliceParticle2DSystem_SetRotatePerSecondVar(lua_State*L)
+{
+	Alice::Particle2DSystem * ptr = (Alice::Particle2DSystem*)lua_touserdata(L, 1);
+	((ParticleSystemQuad*)ptr->node)->setRotatePerSecondVar(lua_tonumber(L, 2));
+	return 0;
+}
+
 namespace Alice
 {
 	Particle2DSystem::Particle2DSystem()
 	{
-		node = ParticleGalaxy::create();
+		//default to create a file particle
+		node = ParticleFire::create();
 	}
+
 	int Particle2DSystem::Init(lua_State*L)
 	{
 		luaL_Reg api_m[] = {
 			{ "AddChild",AliceParticle2DSystem_AddChild },
+			{ "ChangeMode",AliceParticle2DSystem_ChangeMode },
+			//重力模式
+			{ "SetGravity",AliceParticle2DSystem_SetGravity },
+			{ "SetSpeed",AliceParticle2DSystem_SetSpeed },
+			{ "SetSpeedVar",AliceParticle2DSystem_SetSpeedVar },
+			{ "SetTangencialAccel",AliceParticle2DSystem_SetTangencialAccel },
+			{ "SetTangencialAccelVar",AliceParticle2DSystem_SetTangencialAccelVar },
+			{ "SetRadialAccel",AliceParticle2DSystem_SetRadialAccel },
+			{ "SetRadialAccelVar",AliceParticle2DSystem_SetRadialAccelVar },
+			//角度模式
+			{ "SetStartRadius",AliceParticle2DSystem_SetStartRadius },
+			{ "SetStartRadiusVar",AliceParticle2DSystem_SetStartRadiusVar },
+			{ "SetEndRadius",AliceParticle2DSystem_SetEndRadius },
+			{ "SetEndRadiusVar",AliceParticle2DSystem_SetEndRadiusVar },
+			{ "SetRotatePerSecond",AliceParticle2DSystem_SetRotatePerSecond },
+			{ "SetRotatePerSecondVar",AliceParticle2DSystem_SetRotatePerSecondVar },
+			//公用属性
+
 			{ NULL,NULL }
 		};
 		luaL_newmetatable(L, "Particle2DSystemT");
