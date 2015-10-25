0e1249f9fc7026940a6f35c43a3c9ed144979b74
Heck <zgzamoshi@163.com>
2015-10-25 13:46:04 +0800

    更新
    Signed-off-by: Heck <zgzamoshi@163.com>

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -1,21 +1,12 @@
 #include "main.h"
 #include "AppDelegate.h"
 #include "cocos2d.h"
+#include "LuaEngine.h"
 
 USING_NS_CC;
 
 #if _MSC_VER > 1800
 #pragma comment(lib,"libcocos2d_2015.lib")
-#pragma comment(lib,"libbox2d_2015.lib")
-#pragma comment(lib,"libSpine_2015.lib")
-#pragma comment(lib,"libbullet_2015.lib")
-#pragma comment(lib,"librecast_2015.lib")
-#else
-#pragma comment(lib,"libcocos2d_2013.lib")
-#pragma comment(lib,"libbox2d_2013.lib")
-#pragma comment(lib,"libSpine_2013.lib")
-#pragma comment(lib,"libbullet_2013.lib")
-#pragma comment(lib,"librecast_2013.lib")
 #endif
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
@@ -25,7 +16,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
-
+	Alice::LuaEngine::Init();
     // create the application instance
     AppDelegate app;
     return Application::getInstance()->run();
